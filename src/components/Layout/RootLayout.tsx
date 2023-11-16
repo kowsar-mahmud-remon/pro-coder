afed06d64f0fb4c733e0c8882ac483b5eae62df7
creativerze <creativerze@gmail.com>
2023-11-17 02:02:41 +0600

    added footer

--- src/components/Layout/RootLayout.tsx
+++ src/components/Layout/RootLayout.tsx
@@ -1,11 +1,13 @@
 import { Toaster } from "react-hot-toast";
 import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 const RootLayout = ({ children }: any) => {
   return (
     <>
       <Navbar></Navbar>
       {children}
+      <Footer></Footer>
       <Toaster />
     </>
   );
